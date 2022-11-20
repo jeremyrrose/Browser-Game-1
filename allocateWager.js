f0e1f1cf253013a25fd03c8d302fa0475cd832e2
JustSlamThings <justslamthings@gmail.com>
2022-11-20 14:16:22 -0500

    Buttons except for deal is good now

--- allocateWager.js
+++ allocateWager.js
@@ -1,28 +1,78 @@
-var playerBet= document.getElementById("playerBet")
+let playerBet= document.getElementById("playerBet")
+let playerBetButton=document.getElementById("playerBetButton");
+playerBetButton.addEventListener('click',betOnPlayer)
 
-var bankerBet= document.getElementById("bankerBet")
+let bankerBet = document.getElementById("bankerBet");
+let bankerBetButton=document.getElementById("bankerBetButton");
+bankerBetButton.addEventListener('click',betOnBanker)
 
-var tieBet= document.getElementById("tieBet")
+let tieBet= document.getElementById("tieBet");
+let tieBetButton=document.getElementById("tieBetButton");
+tieBetButton.addEventListener('click',betOnTie)
 
+let clearAllButton=document.getElementById("clearAll");
+clearAllButton.addEventListener('click',resetBets)
 
-var betAmount=document.getElementById("amountwagered")
+var betAmount=document.getElementById("amountWagered")
 
 function betOnBanker(){
-bankerBet.innerText=amountwagered.value
+    resetBets();    
+    bankerBet.innerText=amountwagered.value;
 }
 
 
 
 function betOnPlayer(){
-playerBet.innerText=amountwagered.value
+    resetBets();
+playerBet.innerText=amountwagered.value;
 }
 
 
 function betOnTie(){
-tieBet.innerText=amountwagered.value
+resetBets();
+    tieBet.innerText=amountwagered.value;
+
 }
 
 function resetBets(){
-betAmount.value=0
+tieBet.innerText=""
+playerBet.innerText=""
+bankerBet.innerText=""
 }
 
+
+// let playerBet= document.getElementById("playerBet")
+// playerBet.addEventListener('click', event => {
+//   betOnPlayer();
+// });
+
+// let bankerBet = document.getElementById("bankerBet");
+// bankerBet.addEventListener('click', event => {
+//   test();
+// });
+// let tieBet= document.getElementById("tieBet");
+// tieBet.addEventListener('click', event => {
+//   test();
+// });
+
+// var betAmount=document.getElementById("amountwagered")
+
+// function betOnBanker(){
+// bankerBet.innerText=amountwagered.value;
+
+// }
+
+
+
+// function betOnPlayer(){
+// playerBet.innerText=amountwagered.value
+// }
+
+
+// function betOnTie(){
+// tieBet.innerText=amountwagered.value
+// }
+
+// function resetBets(){
+// betAmount.value=0
+// }
