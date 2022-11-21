45f467cf62ac6e05e7f84df96f2c30db1ec1b491
JustSlamThings <49546639+JustSlamThings@users.noreply.github.com>
2022-11-21 16:02:18 -0500

    Add files via upload
    Chips can add values now! Yeeeeeeeee

--- index.js
+++ index.js
@@ -1,4 +1,43 @@
 
+const whitechipBTN = document.getElementById("white"); // 1
+const bluechipBTN = document.getElementById("blue"); // 5
+const greenchipBTN = document.getElementById("green"); // 25
+const redchipBTN = document.getElementById("red"); // 50 
+const blackchipBTN = document.getElementById("black"); //100
+let waggerinput=document.getElementById("amountwagered");
+
+blackchipBTN.addEventListener('click', function(){
+    betamount(100);
+});
+whitechipBTN.addEventListener('click', function(){
+    betamount(1);
+});
+bluechipBTN.addEventListener('click', function(){
+    betamount(5);
+});
+greenchipBTN.addEventListener('click', function(){
+    betamount(25);
+});
+redchipBTN.addEventListener('click', function(){
+    betamount(50);
+});
+let y=null;
+
+function betamount(chipval){
+    console.log("chip clicked:"+ chipval);
+    console.log(y)
+    // if (y=null){
+    let x= parseInt(waggerinput.value);
+    // else{
+    //     x=
+    // }
+    waggerinput.value =  x+chipval;
+    y=waggerinput.value
+    console.log(y)
+}
+// function blackchipBTN(){
+// blackchipBTN.addEventListener('click',betamount(100));
+// }
 // //Build initial Class 
 // class cards{
 // 	constructor(name,age){
